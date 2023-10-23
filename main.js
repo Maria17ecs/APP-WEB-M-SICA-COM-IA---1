cfc2a980a32b2ad9ff053514fe259001d256b989
Maria17ecs <122824717+Maria17ecs@users.noreply.github.com>
2023-10-23 19:30:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song = "";
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
